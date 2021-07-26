daab3b4e1255a04aea95c51f9429ee8318a4da9f
yrojx <yossansr06@gmail.com>
2021-07-26 13:44:15 +0700

    feat: add XIB constants

---
+++ Peek-a-Book/Helper/XIBConstant.swift
@@ -0,0 +1,16 @@
+//
+//  XIBConstant.swift
+//  Peek-a-Book
+//
+//  Created by Yossan Sandi Rahmadi on 26/07/21.
+//
+
+import Foundation
+
+struct XIBConstant {
+    static let BooksVC = "BooksViewController"
+    static let ProfileVC = "ProfileViewController"
+}
+
+
+
