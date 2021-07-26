d77e9561f577e029e1f6c925d1a13c1f11b1e7a2
yrojx <yossansr06@gmail.com>
2021-07-26 13:44:39 +0700

    feat: add books coordinator

---
+++ Peek-a-Book/Module/Books/Coordinator/BooksCoordinator.swift
@@ -0,0 +1,20 @@
+//
+//  BooksCoordinator.swift
+//  Peek-a-Book
+//
+//  Created by Yossan Sandi Rahmadi on 26/07/21.
+//
+
+import UIKit
+
+class BooksCoordinator: BooksBaseCoordinator {
+    var rootViewController: UIViewController = UIViewController()
+    
+    var parentCoordinator: MainBaseCoordinator?
+    
+    func start() -> UIViewController {
+        rootViewController = UINavigationController(rootViewController: BooksViewController(coordinator: self, nibName: XIBConstant.BooksVC))
+        
+        return rootViewController
+    }
+}
