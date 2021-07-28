3c0bc8fa49d820d257732a06ce9916430a3586f7
PAW123 <gedeprama123@gmail.com>
2021-07-28 16:14:23 +0700

    Reusable view
    XIB reusable view 2 task udah masuk

--- Peek-a-Book/Helper/XIBConstant.swift
+++ Peek-a-Book/Helper/XIBConstant.swift
@@ -10,6 +10,11 @@ import Foundation
 struct XIBConstant {
     static let BooksVC = "BooksViewController"
     static let ProfileVC = "ProfileViewController"
+  
+    
+    static let BooksHomeScreenCVC = "BooksHomescreenCollectionViewCell"
+    static let KatalogLenderCVC = "KatalogLenderCollectionViewCell"
+    
 }
 
 
