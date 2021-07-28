018df9d7daf6c2050b5ac46ba1ff86a03e5058f3
yrojx <yossansr06@gmail.com>
2021-07-29 04:46:18 +0700

    feat: add Network constants
    with example API (jsonplaceholder API)

---
+++ Peek-a-Book/Helper/Constant.swift
@@ -0,0 +1,15 @@
+//
+//  Constant.swift
+//  Peek-a-Book
+//
+//  Created by Yossan Sandi Rahmadi on 28/07/21.
+//
+
+import Foundation
+
+struct Constant {
+    struct Network {
+        static let baseUrl = "https://jsonplaceholder.typicode.com"
+        static let userRoute = "users"
+    }
+}
