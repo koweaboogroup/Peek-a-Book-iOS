c2fd220d19bb366bb86c4966dc0db63d19231b7a
Annetta Carolina <annetta288@ymail.com>
2021-07-30 17:51:10 +0700

    VIEWMODEL LOGIN KELAAARRRRR

--- Peek-a-Book/Module/Login/ViewModel/LoginViewModel.swift
+++ Peek-a-Book/Module/Login/ViewModel/LoginViewModel.swift
@@ -9,10 +9,41 @@ import Foundation
 import RxSwift
 
 class LoginViewModel {
-    public let identifier : PublishSubject<String> = PublishSubject()
-    public let password : PublishSubject<String> = PublishSubject()
     public let user : PublishSubject<User> = PublishSubject()
     public let loading : PublishSubject<Bool> = PublishSubject()
+    public let error : PublishSubject<String> = PublishSubject()
     
     public let buttonLoginPressed = PublishSubject<Bool>()
+    
+    
+    
+    public func login(identifier: String, password: String) {
+        
+        self.loading.onNext(true)
+        
+        let loginRequest = LoginRequest(identifier: identifier, password: password)
+        
+        LoginService.login(loginRequest: loginRequest) { loginResponse in
+            self.loading.onNext(false)
+            
+            //kalo success
+            let jwtUserDefaults = "jwt"
+            UserDefaults.standard.string(forKey: jwtUserDefaults)
+            UserDefaults.standard.set(loginResponse.jwt, forKey: jwtUserDefaults)
+            
+            if let responseUser = loginResponse.user {
+                self.user.onNext(responseUser)
+            } else {
+                self.error.onNext("Data Tidak Ditemukan")
+            }
+            
+        } failCompletion: { error in
+            
+            self.loading.onNext(false)
+            self.error.onNext(error.errorDescription ?? "Error")
+            fatalError()
+            
+        }
+        
+    }
 }
