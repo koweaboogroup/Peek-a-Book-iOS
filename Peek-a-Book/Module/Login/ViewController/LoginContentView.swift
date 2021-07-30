7b4374965a78e2b12b3eb4f78588fe6127c08136
yrojx <yossansr06@gmail.com>
2021-07-30 21:25:53 +0700

    feat: add LoginContentView

---
+++ Peek-a-Book/Module/Login/ViewController/LoginContentView.swift
@@ -0,0 +1,65 @@
+//
+//  LoginContentView.swift
+//  Peek-a-Book
+//
+//  Created by Yossan Sandi Rahmadi on 29/07/21.
+//
+
+import UIKit
+
+@IBDesignable
+
+class LoginContentView: UIView {
+    
+    @IBOutlet weak var loginContentView: UIView!
+    
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var fieldContainer: UIView!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        commonInit()
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+    }
+    
+    override func awakeFromNib() {
+        layer.cornerRadius = 36
+        layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
+        clipsToBounds = true
+    }
+    
+    func commonInit() {
+        loginContentView = loadViewFromNib(nibName: XIBConstant.LoginContentView)
+        loginContentView.frame = self.bounds
+        
+        passwordTextField.isSecureTextEntry = true
+        
+        loginButton.layer.cornerRadius = 12
+        loginButton.layer.applyShadow(
+            color: .black,
+            alpha: 0.2,
+            x: 0,
+            y: 3,
+            blur: 10,
+            spread: 0
+        )
+        
+        fieldContainer.layer.cornerRadius = 12
+        fieldContainer.layer.applyShadow(
+            color: .black,
+            alpha: 0.1,
+            x: 0,
+            y: 2,
+            blur: 5,
+            spread: 0
+        )
+        
+        self.addSubview(loginContentView)
+    }
+}
