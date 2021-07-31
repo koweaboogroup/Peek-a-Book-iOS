61e89d8842601aa1620025dd3d52303325fca0d1
yrojx <yossansr06@gmail.com>
2021-08-01 02:05:19 +0700

    feat: setup subscriber and loginView responsiveness

--- Peek-a-Book/Module/Login/ViewController/LoginViewController.swift
+++ Peek-a-Book/Module/Login/ViewController/LoginViewController.swift
@@ -6,43 +6,80 @@
 //
 
 import UIKit
+import RxSwift
 
 class LoginViewController: UIViewController {
+    
+    @IBOutlet weak var loginViewContent: LoginContentView!
+    
+    @IBOutlet weak var loginViewContentBottomConstraint: NSLayoutConstraint!
+    
+    let viewModel = LoginViewModel()
+    let disposeBag = DisposeBag()
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
 
-        // Hide the navigation bar on the this view controller
         self.navigationController?.setNavigationBarHidden(true, animated: animated)
     }
     
+    private func setupSubcribe() {
+        viewModel.error.subscribe(onNext: { error in
+            let alert = UIAlertController(title: "Tidak berhasil login", message: "Coba dinget2 lagi, yok bisa yok", preferredStyle: .alert)
+            
+            alert.addAction(UIAlertAction(title: "Baiklah!", style: .default, handler: { action in
+                self.loginViewContent.identifierTextField.becomeFirstResponder()
+            }))
+            
+            self.present(alert, animated: true, completion: nil)
+        }).disposed(by: disposeBag)
+        
+        viewModel.user.subscribe(onNext: { user in
+            self.changeToProfileVC()
+        }).disposed(by: disposeBag)
+    }
+    
+    private func changeToProfileVC() {
+        let profileVC = UINavigationController(rootViewController: ProfileViewController())
+        profileVC.tabBarItem = UITabBarItem(title: "Profile", image: UIImage(systemName: "person.crop.circle.fill"), tag: 1)
+
+        self.tabBarController?.viewControllers?[1] = profileVC
+    }
+    
+    private func setupKeyboardListener() {
+        NotificationCenter.default.addObserver(self, selector: #selector(handleKeyboardNotification), name: UIResponder.keyboardWillShowNotification, object: nil)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(handleKeyboardNotification), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        loginViewContent.initViewModel(viewModel)
+        
         self.hideKeyboardWhenTappedAround()
-        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
-        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+        
+        setupKeyboardListener()
+        
+        setupSubcribe()
     }
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
 
-        // Show the navigation bar on other view controllers
         self.navigationController?.setNavigationBarHidden(false, animated: animated)
     }
     
-    @objc func keyboardWillShow(notification: NSNotification) {
-        if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
-            if self.view.frame.origin.y == 0 {
-                self.view.frame.origin.y -= keyboardSize.height
-            }
-        }
-    }
+    @objc func handleKeyboardNotification(notification: NSNotification) {
+        if let userInfo = notification.userInfo {
+            let keyboardFrameValue = (userInfo[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)
+            let keyboardFrame = keyboardFrameValue?.cgRectValue
 
-    @objc func keyboardWillHide(notification: NSNotification) {
-        if self.view.frame.origin.y != 0 {
-            self.view.frame.origin.y = 0
+            let isKeyboardShowing = notification.name == UIResponder.keyboardWillShowNotification
+
+            loginViewContentBottomConstraint.constant = isKeyboardShowing ? keyboardFrame!.height - keyboardFrame!.height * 0.3 : 4
+            
+            self.view.layoutIfNeeded()
         }
     }
 
