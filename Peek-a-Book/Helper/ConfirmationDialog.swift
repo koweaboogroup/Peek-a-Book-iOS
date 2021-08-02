3b6407499b7ef0f834b73987a9fa963a7578c0f7
rahmanarifofficial <rahmanarifofficial@gmail.com>
2021-08-02 19:05:42 +0700

    Penambahan general alert dialog

---
+++ Peek-a-Book/Helper/ConfirmationDialog.swift
@@ -0,0 +1,39 @@
+//
+//  ConfirmationDialog.swift
+//  Peek-a-Book
+//
+//  Created by Arif Rahman on 02/08/21.
+//
+
+import Foundation
+import UIKit
+class ConfirmationDialog {
+    static func showAlert(viewController: UIViewController, title: String, subtitle: String, positiveText: String, negativeText: String, positiveCompletion: @escaping () -> Void, negativeCompletion: @escaping () -> Void){
+        let alert = UIAlertController(
+            title: title,
+            message: subtitle,
+            preferredStyle: .alert
+        )
+        alert.addAction(UIAlertAction(title: negativeText, style: .destructive) { _ in
+            negativeCompletion()
+        })
+        
+        alert.addAction(UIAlertAction(title: positiveText, style: .default) { _ in
+            positiveCompletion()
+        })
+        
+        viewController.present(alert, animated: true)
+    }
+    
+    static func showInfoAlert(viewController: UIViewController, title: String, subtitle: String, text: String, completion: @escaping () -> Void){
+        let alert = UIAlertController(
+            title: title,
+            message: subtitle,
+            preferredStyle: .alert
+        )
+        alert.addAction(UIAlertAction(title: text, style: .cancel) { _ in
+            completion()
+        })
+        viewController.present(alert, animated: true)
+    }
+}
