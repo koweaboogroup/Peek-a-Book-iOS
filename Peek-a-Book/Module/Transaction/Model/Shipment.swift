8e9101e3ed3c62526d3ad1ed1206ba16fa5f8e4f
Annetta Carolina <annetta288@ymail.com>
2021-08-03 17:29:49 +0700

    Added enum classes for transaction

---
+++ Peek-a-Book/Module/Transaction/Model/Shipment.swift
@@ -0,0 +1,14 @@
+//
+//  Shipment.swift
+//  Peek-a-Book
+//
+//  Created by Annetta Carolina on 03/08/21.
+//
+
+import Foundation
+
+enum Shipment: String {
+    case selfPickup = "Self Pickup"
+    case COD = "COD"
+    case kurir = "Kurir"
+}
