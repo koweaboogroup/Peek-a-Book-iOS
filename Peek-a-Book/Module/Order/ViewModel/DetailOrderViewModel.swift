f9b754d8490fe7235dfa35f9c4a3e74efe272829
Yossan Rahmadi <yossansr06@gmail.com>
2021-08-05 06:25:58 +0700

    feat: get detail order

---
+++ Peek-a-Book/Module/Order/ViewModel/DetailOrderViewModel.swift
@@ -0,0 +1,28 @@
+//
+//  DetailOrderViewModel.swift
+//  Peek-a-Book
+//
+//  Created by Yossan Sandi Rahmadi on 05/08/21.
+//
+
+import Foundation
+import RxSwift
+
+struct DetailOrderViewModel {
+    let loading: PublishSubject<Bool> = PublishSubject()
+    let error: PublishSubject<String> = PublishSubject()
+    
+    let order: PublishSubject<Order> = PublishSubject()
+    
+    func getDetailOrder(orderId: Int) {
+        self.loading.onNext(true)
+        
+        OrderService.getDetailOrder(orderId: orderId) { order in
+            self.order.onNext(order)
+            self.loading.onNext(false)
+        } failCompletion: { error in
+            self.loading.onNext(false)
+            self.error.onNext(error.errorDescription ?? "Error")
+        }
+    }
+}
