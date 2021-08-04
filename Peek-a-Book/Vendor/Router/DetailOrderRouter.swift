54d2ac187ae8c149a05d9b7178a7be5c70c7c370
Yossan Rahmadi <yossansr06@gmail.com>
2021-08-05 06:25:08 +0700

    feat: detail order router with get method

---
+++ Peek-a-Book/Vendor/Router/DetailOrderRouter.swift
@@ -0,0 +1,43 @@
+//
+//  DetailOrderRouter.swift
+//  Peek-a-Book
+//
+//  Created by Yossan Sandi Rahmadi on 05/08/21.
+//
+
+import Alamofire
+
+enum DetailOrderRouter: URLRequestConvertible {
+    
+    case get(orderId: Int)
+    
+    var method: HTTPMethod {
+        switch self {
+        case .get: return .get
+        }
+    }
+    
+    var baseUrl: URL {
+        return URL(string: Constant.Network.baseUrl + "rents")!
+    }
+    
+    var path: String {
+        switch self {
+        case .get: return "rents"
+        }
+    }
+    
+    func asURLRequest() throws -> URLRequest {
+        let url = baseUrl.appendingPathComponent(path)
+        
+        var request = URLRequest(url: url)
+        
+        request.headers.add(.contentType("application/json"))
+        request.headers.add(.accept("application/json"))
+        
+        request.method = method
+        
+        return request
+    }
+    
+}
