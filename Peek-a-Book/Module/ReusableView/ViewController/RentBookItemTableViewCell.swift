5bb29d70494efd1c7f6910517abfb6b23dd9d3f6
Muhammad Rifki Widadi <61343742+mrifkiw@users.noreply.github.com>
2021-08-04 22:52:24 +0700

    feat: add reusable view for daftar penyewaan

---
+++ Peek-a-Book/Module/ReusableView/ViewController/RentBookItemTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  RentBookItemView.swift
+//  Peek-a-Book
+//
+//  Created by Muhammad Rifki Wildadi on 04/08/21.
+//
+
+import UIKit
+
+class RentBookItemTableViewCe: UITableViewCell{
+    
+    @IBOutlet weak var userProfileImage: CircleImageView!
+    
+    @IBOutlet weak var lendersName: UILabel!
+}
