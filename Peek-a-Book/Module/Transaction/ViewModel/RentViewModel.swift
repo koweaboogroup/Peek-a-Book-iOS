c4e7bfb6eb4a2ffa35307a2b816e5931cd1ae3e4
Muhammad Rifki Widadi <61343742+mrifkiw@users.noreply.github.com>
2021-08-06 00:44:27 +0700

    feat: add view model for rent

--- Peek-a-Book/Module/Transaction/ViewModel/RentViewModel.swift
+++ Peek-a-Book/Module/Transaction/ViewModel/RentViewModel.swift
@@ -10,4 +10,16 @@ import RxSwift
 
 struct RentViewModel{
     public let rent: PublishSubject<[RentResponse]> = PublishSubject()
+    let error: PublishSubject<String> = PublishSubject()
+    
+    func getListRentAsUser(id: String){
+        RentService.getListRentTransaction(id: id) { rentResponse in
+            self.rent.onNext(rentResponse)
+        } failCompletion: { error in
+            self.error.onNext(error.errorDescription!)
+        }
+    }
+    
+    
+    
 }
