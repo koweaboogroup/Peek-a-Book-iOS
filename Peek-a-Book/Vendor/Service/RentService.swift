9344a5e48b067997c5b0dfec8b92f3a0eece6dd8
Muhammad Rifki Widadi <61343742+mrifkiw@users.noreply.github.com>
2021-08-06 00:45:37 +0700

    feat: add variable error in rentservices

--- Peek-a-Book/Vendor/Service/RentService.swift
+++ Peek-a-Book/Vendor/Service/RentService.swift
@@ -9,14 +9,14 @@ import Alamofire
 
 class RentService {
     
-    static func getListRentTransaction(id: Int, successCompletion: @escaping (RegisterResponse) -> Void, failCompletion: @escaping (AFError) -> Void) {
+    static func getListRentTransaction(id: String, successCompletion: @escaping ([RentResponse]) -> Void, failCompletion: @escaping (AFError) -> Void) {
         
-        BaseRequest.post(router: RegisterRouter.post(registerRequest)) { request in
-            request.responseDecodable(of: RegisterResponse.self) { response in
+        BaseRequest.get(router: RentRouter.get(id: id)) { request in
+            request.responseDecodable(of: [RentResponse].self) { response in
                 
                 switch response.result {
-                case .success(let registerResponse):
-                    successCompletion(registerResponse)
+                case .success(let rentResponse):
+                    successCompletion(rentResponse)
                 case .failure(let error):
                     failCompletion(error)
                 }
