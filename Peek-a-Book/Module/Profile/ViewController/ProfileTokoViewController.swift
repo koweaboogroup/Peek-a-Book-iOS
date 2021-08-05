fd06e9d26474e94d3c5c8f9cdee1f3b54fe36fcd
PAW123 <gedeprama123@gmail.com>
2021-08-05 18:35:32 +0700

    View untuk profile toko telah selesai

---
+++ Peek-a-Book/Module/Profile/ViewController/ProfileTokoViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ProfileTokoViewController.swift
+//  Peek-a-Book
+//
+//  Created by Gede Wicaksana on 05/08/21.
+//
+
+import UIKit
+
+class ProfileTokoViewController: UIViewController {
+
+    @IBOutlet weak var profilTokoImage: UIImageView!
+    @IBOutlet weak var namaTokoLabel: UILabel!
+    @IBOutlet weak var bioTokoLabel: UILabel!
+    
+    @IBOutlet weak var lokasiTokoLabel: UILabel!
+    
+    @IBOutlet weak var koleksiBukuCollectionView: UICollectionView!
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Edit Profile", style: .plain, target: self, action: #selector(addTapped))
+
+    }
+    
+    
+    
+    
+    
+    @objc func addTapped(){
+         print("clicked")
+    }
+    
+
+
+    @IBAction func semuaGenrePressed(_ sender: UIButton) {
+        print("Sort Disini")
+    }
+    
+}
