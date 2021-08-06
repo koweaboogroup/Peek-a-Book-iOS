a241aee7a51af90da17b777126deb9d82410bdec
Annetta Carolina <annetta288@ymail.com>
2021-08-06 11:01:11 +0700

    Added some bindings for Lender Profile

--- Peek-a-Book/Module/ProfileLender/ViewController/ProfileTokoViewController.swift
+++ Peek-a-Book/Module/ProfileLender/ViewController/ProfileTokoViewController.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class ProfileTokoViewController: UIViewController {
 
@@ -17,16 +19,47 @@ class ProfileTokoViewController: UIViewController {
     
     @IBOutlet weak var koleksiBukuCollectionView: UICollectionView!
     
-    
+    private let viewModel = ProfileLenderViewModel()
+    private let disposeBag = DisposeBag()
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Edit Profile", style: .plain, target: self, action: #selector(addTapped))
+        
+        setupRx()
 
     }
     
+    private func setupRx() {
+        
+        //MARK: - Setup View Detail Toko
+        viewModel.lenderProfile.subscribe (onNext: { response in
+            let url = URL(string: Constant.Network.baseUrl + (response.image?[0].url ?? ""))
+            self.profilTokoImage.kf.setImage(with: url)
+        }).disposed(by: disposeBag)
+        
+        viewModel.lenderProfile.asObserver().map { response in
+            response.name
+        }.bind(to: namaTokoLabel.rx.text)
+        .disposed(by: disposeBag)
+        
+        viewModel.lenderProfile.asObserver().map { response in
+            response.bio
+        }.bind(to: bioTokoLabel.rx.text)
+        .disposed(by: disposeBag)
+        
+        //MARK: Ini mapping lokasinya mau pake Kota aja ya?
+        viewModel.lenderProfile.asObserver().map { response in
+            response.kota
+        }.bind(to: lokasiTokoLabel.rx.text)
+        .disposed(by: disposeBag)
+        
+        //MARK: - Setup View Katalog Toko
+        
+        
+    }
     
     
     
