b3e3adaceaba57d1c790a145dd45da7d1649aa6b
Yossan Rahmadi <yossansr06@gmail.com>
2021-08-18 20:20:20 +0700

    refactor: delete unused func

--- Peek-a-Book/Module/Order/View Controller/DetailOrderViewController.swift
+++ Peek-a-Book/Module/Order/View Controller/DetailOrderViewController.swift
@@ -65,10 +65,6 @@ class DetailOrderViewController: UIViewController {
         self.orderId = orderId
     }
     
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         setNavigationBar()
