fb0d8af1d85a62c04cca70c41c25c0a102ab6f13
Yossan Rahmadi <yossansr06@gmail.com>
2021-08-18 20:17:18 +0700

    feat: add server attribute name getter

--- Peek-a-Book/Vendor/Model/Shipment.swift
+++ Peek-a-Book/Vendor/Model/Shipment.swift
@@ -9,6 +9,17 @@ import Foundation
 
 enum Shipment: String {
     case selfPickup = "Self Pickup"
-    case COD = "COD"
+    case cod = "COD"
     case kurir = "Kurir"
+    
+    func getServerAttributeName() -> String {
+        switch self {
+        case .selfPickup:
+            return "self_pickup"
+        case .cod:
+            return "cod"
+        case .kurir:
+            return "kurir"
+        }
+    }
 }
