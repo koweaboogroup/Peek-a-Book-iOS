925ca595206633c0d4a92a1af1c09e014610ad45
Muhammad Rifki Widadi <61343742+mrifkiw@users.noreply.github.com>
2021-08-26 16:54:14 +0700

    feat: add reusableview for error state

---
+++ Peek-a-Book/Module/ReusableView/ViewController/ErrorStateView.swift
@@ -0,0 +1,16 @@
+//
+//  ErrorStateView.swift
+//  Peek-a-Book
+//
+//  Created by Muhammad Rifki Wildadi on 26/08/21.
+//
+
+import Foundation
+
+@IBDesignable
+class ErrorStateView: UIView {
+    @IBOutlet weak var errorImage: UIImageView!
+    @IBOutlet weak var errorLabel: UILabel!
+    
+    
+}
